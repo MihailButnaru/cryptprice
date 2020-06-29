dfe1821a93a3c52c805a768d05e697ba38d0db56
Mihail Butnaru <butnarum@hotmail.com>
2020-06-29 21:14:35 +0100

    TASK-2: added comments, license and requirements

--- app/cryptprice/utilities/validators.py
+++ app/cryptprice/utilities/validators.py
@@ -5,8 +5,11 @@ __copyright__ = "2020 All rights reserved!"
 
 
 def validate_verification_token(data: dict) -> bool:
+    """ Validates the verification token from slack """
     return data.get("token") == settings.SLACK_VERIFICATION_TOKEN
 
 
 def verification_challenge(data: dict) -> bool:
+    """ Slack sends a verification challenge in order to
+    connect with their slack platform (verification done)"""
     return data.get("type") == "url_verification"
